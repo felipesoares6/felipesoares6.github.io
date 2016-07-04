12e0d51fcff4f1b13c216e4b229d8a48f823ee01
felipesoares6 <felipeluizsoares@gmail.com>
2016-07-04 03:58:49 -0300

    email com ajax

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 $(function(){
   $('#formulario').on('submit', function(e){
-    //e.preventDefault();
     var $nome = $('#nome');
     var $email = $('#email');
     var $mensagem = $('#mensagem');
-
-
-    alert('Email enviado com sucesso!');
-    
+    if($nome.val() == '' || $email.val() == '' || $mensagem.val() == ''){
+      alert('Todos os campos devem ser preenchidos!');
+    }else{
+      $.ajax({
+          method: 'POST',
+          url: '//formspree.io/felipeluizsoares@gmail.com',
+          data: $('#formulario').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        alert('Email enviado com sucesso!');
+        $(this).get(0).reset();
+    }
   });
 });
