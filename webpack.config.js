f35c99196fa4a377a3db4363dff96b787220bac1
Felipe Soares <felipeluizsoares@gmail.com>
2019-01-25 02:00:18 -0200

    Update manifest icons path

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              outputPath: 'images',
+              outputPath: 'public/images',
             },
           },
         ],
